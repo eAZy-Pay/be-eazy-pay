ea72e13467c705be445b38ebb5ee3af19b519fc6
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2024-04-08 17:52:11 +0900

    EZ-74 Feat: 6개월 소비내역 통계 API

--- src/main/java/com/eazy/pay/service/MonthlyFor6Service.java
+++ src/main/java/com/eazy/pay/service/MonthlyFor6Service.java
@@ -1,11 +1,14 @@
 package com.eazy.pay.service;
 
+import com.eazy.pay.dto.MonthlyFor6ResponseDTO;
+import com.eazy.pay.mapper.MonthlyFor6ResponseMapper;
 import com.eazy.pay.model.MonthlyFor6;
 import com.eazy.pay.dao.MonthlyFor6Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MonthlyFor6Service {
@@ -13,7 +16,13 @@ public class MonthlyFor6Service {
     @Autowired
     private MonthlyFor6Repository monthlyFor6Repository;
 
-    public List<MonthlyFor6> getAllMonthlyFor6() {
-        return monthlyFor6Repository.findAll();
+
+    public List<MonthlyFor6ResponseDTO> getMonthlyFor6ByUserId(Long userId) {
+
+        List<MonthlyFor6ResponseDTO> monthlyFor6ResponseDTOList = monthlyFor6Repository.findByUserUid(userId).stream()
+                .map(MonthlyFor6ResponseMapper.INSTANCE::toDTO)
+                .collect(Collectors.toList());
+
+        return monthlyFor6ResponseDTOList;
     }
 }
