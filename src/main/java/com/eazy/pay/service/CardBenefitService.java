ee60ac356d62230017589b02172e454f1cba9710
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2024-04-08 17:54:21 +0900

    EZ-75 Feat: 카테고리 카드 목록 API

--- src/main/java/com/eazy/pay/service/CardBenefitService.java
+++ src/main/java/com/eazy/pay/service/CardBenefitService.java
@@ -1,11 +1,15 @@
 package com.eazy.pay.service;
 
 import com.eazy.pay.dao.CardBenefitRepository;
+import com.eazy.pay.dto.CardDTO;
+import com.eazy.pay.mapper.CardMapper;
+import com.eazy.pay.model.Card;
 import com.eazy.pay.model.CardBenefit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CardBenefitService {
@@ -13,7 +17,12 @@ public class CardBenefitService {
     @Autowired
     private CardBenefitRepository cardBenefitRepository;
 
-    public List<CardBenefit> getAllCardBenefits() {
-        return cardBenefitRepository.findAll();
+    public List<CardDTO> getCategoryCards(Long categoryId) {
+        List<CardDTO> cardDTOList = cardBenefitRepository.findByCategoryUid(categoryId).stream()
+                .map(CardBenefit::getCard)
+                .map(CardMapper.INSTANCE::toDTO)
+                .collect(Collectors.toList());
+
+        return cardDTOList;
     }
 }
