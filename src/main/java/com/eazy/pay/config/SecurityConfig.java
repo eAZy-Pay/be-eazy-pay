0b9366477a4b3d4d0123379c9892b5d6d7192ddc
PARK SEONJU <sj.ella1030@gmail.com>
2024-04-12 11:12:48 +0900

    Fix: CORS Origin 정보 수정

--- src/main/java/com/eazy/pay/config/SecurityConfig.java
+++ src/main/java/com/eazy/pay/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://127.0.0.1:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://839f-118-131-63-237.ngrok-free.app"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
