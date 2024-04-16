921ddb8fd24a51d23cf1e35a769b81dbcd342e70
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2024-04-16 20:25:10 +0900

    EZ-82 Feat: 카드 이름으로 검색 기능

--- src/main/java/com/eazy/pay/service/CardService.java
+++ src/main/java/com/eazy/pay/service/CardService.java
@@ -1,10 +1,14 @@
 package com.eazy.pay.service;
+import com.eazy.pay.dto.CardDTO;
+import com.eazy.pay.mapper.CardMapper;
 import com.eazy.pay.model.Card;
 import com.eazy.pay.dao.CardRepository;
+import com.eazy.pay.model.CardBenefit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CardService {
@@ -14,5 +18,13 @@ public class CardService {
 
     public List<Card> getAllCards() {
         return cardRepository.findAll();
+    }
+
+    public List<CardDTO> getCardsLikeName(String name) {
+        List<CardDTO> cardDTOList = cardRepository.findByNameContaining(name).stream()
+                .map(CardMapper.INSTANCE::toDTO)
+                .collect(Collectors.toList());
+
+        return cardDTOList;
     }
 }
