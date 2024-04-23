1a9aec0dd4219f0b471fde0b38a14710ce725685
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2024-04-23 12:31:59 +0900

    Chore: Woori Card 관련 파일 수정

--- src/main/java/com/eazy/pay/dao/WooriCardUserRepository.java
+++ src/main/java/com/eazy/pay/dao/WooriCardUserRepository.java
@@ -1,7 +1,12 @@
 package com.eazy.pay.dao;
 
+import com.eazy.pay.model.WooriCardUser;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface WooriCardUserRepository {
+public interface WooriCardUserRepository extends JpaRepository<WooriCardUser, Long> {
+    List<WooriCardUser> findAll();
 }
