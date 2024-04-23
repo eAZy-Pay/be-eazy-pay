a42815305b40e41268723187b9730895c12d7bff
PARK SEONJU <sj.ella1030@gmail.com>
2024-04-23 19:49:09 +0900

    Feat: UserCardHistory엔티티 추가

---
+++ src/main/java/com/eazy/pay/model/UserCardHistory.java
@@ -0,0 +1,27 @@
+package com.eazy.pay.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "user_card_histories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+public class UserCardHistory extends BaseEntity{
+    @ManyToOne
+    @JoinColumn(name = "user_card_id", referencedColumnName = "uid")
+    private UserCard userCard;
+    private Date yearAndMonth;
+    private Integer benefitAmount;
+    private Integer useAmount;
+    private Boolean is_fulfilled;
+}
