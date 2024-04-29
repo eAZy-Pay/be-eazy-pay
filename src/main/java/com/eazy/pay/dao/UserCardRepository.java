3a3becb70e9c24b9432843eba623af76765fd24c
jimin <jimincheon76@gmail.com>
2024-04-29 16:19:57 +0900

    Chore: DTO 필드 수정

--- src/main/java/com/eazy/pay/dao/UserCardRepository.java
+++ src/main/java/com/eazy/pay/dao/UserCardRepository.java
@@ -24,7 +24,7 @@ public interface UserCardRepository extends JpaRepository<UserCard, Long> {
     List<UserCard> findByUserUid(Long userUid);
 
     // PaymentHistory 정보 조회
-    @Query("SELECT new com.eazy.pay.dto.PaymentHistoryDTO(pb.paymentDate, pb.paymentAmount, pb.storeName) " +
+    @Query("SELECT new com.eazy.pay.dto.PaymentHistoryDTO(pb.paymentDate, pb.paymentAmount, pb.storeName, pb.cardNum) " +
             "FROM UserCard uc " +
             "JOIN uc.user u " +
             "LEFT JOIN PaymentHistory pb ON pb.cardNum = uc.num " +
