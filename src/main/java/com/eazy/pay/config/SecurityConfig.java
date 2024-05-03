673fdd83c6a6afa1bb41e228b9ec4392f1f4c56d
jimin326 <151506984+jimin326@users.noreply.github.com>
2024-05-04 02:18:14 +0900

    Update SecurityConfig.java

--- src/main/java/com/eazy/pay/config/SecurityConfig.java
+++ src/main/java/com/eazy/pay/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://api.eazy-pay.online", "http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://www.eazy-pay.online", "http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
